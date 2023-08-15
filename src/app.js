5e43ea6d2fa9effb0664d8ce81fc370d2afcbb22
Ajit Bohra <ajit@lubus.in>
2023-08-15 23:47:35 +0530

    Add steps list base ui

--- src/app.js
+++ src/app.js
@@ -1,26 +1,86 @@
+import { useState } from '@wordpress/element';
 import {
+    Button,
     Card,
     CardHeader,
     CardMedia,
+    __experimentalHeading as Heading,
+    __experimentalItemGroup as ItemGroup,
+    __experimentalItem as Item,
     __experimentalGrid as Grid,
-    __experimentalText as Text,
+    __experimentalView as View,
+    __experimentalHStack as HStack,
+    __experimentalVStack as VStack,
+    __experimentalConfirmDialog as ConfirmDialog
 } from '@wordpress/components';
+import { Icon, edit, closeSmall, cancelCircleFilled } from "@wordpress/icons"
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import sampleBluePrint from './sample-blueprint';
 import Header from './header';
 import Steps from './steps';
 
 function App() {
+    const [isOpen, setOpen] = useState(false);
+
     return (
         <>
             <Card>
                 <CardHeader id="app-header">
-                    <Header blueprint={sampleBluePrint}/>
+                    <Header blueprint={sampleBluePrint} />
                 </CardHeader>
                 <CardMedia>
                     <Grid columns={3} templateColumns="repeat(2, 1fr) 2fr">
                         <Steps />
-                        <Text>Steps List</Text>
+                        <ItemGroup
+                            isBordered
+                            isRounded
+                            isSeparated
+                            size="large"
+                            style={{ marginTop: "10px" }}
+                        >
+                            <Item>
+                                <HStack>
+                                    <View>login</View>
+                                    <HStack alignment='right' expanded={false}>
+                                        <Button icon={edit} size="compact" />
+                                        <Button
+                                            icon={closeSmall}
+                                            size="compact"
+                                            onClick={() => setOpen(true)}
+                                        />
+                                        <ConfirmDialog
+                                            isOpen={isOpen}
+                                            onConfirm={() => {
+                                                alert('Delete Confirmed!');
+                                                setOpen(false);
+                                            }}
+                                            onCancel={() => setOpen(false)}>
+                                            <HStack>
+                                                <Heading level={4}>Would you like to Delete this step?</Heading>
+                                            </HStack>
+                                        </ConfirmDialog>
+                                    </HStack>
+                                </HStack>
+                            </Item>
+                            <Item>
+                                <HStack>
+                                    <View>installPlugin</View>
+                                    <HStack alignment='right' expanded={false}>
+                                        <Button icon={edit} size="compact" />
+                                        <Button icon={closeSmall} size="compact" />
+                                    </HStack>
+                                </HStack>
+                            </Item>
+                            <Item>
+                                <HStack>
+                                    <View>installTheme</View>
+                                    <HStack alignment='right' expanded={false}>
+                                        <Button icon={edit} size="compact" />
+                                        <Button icon={closeSmall} size="compact" />
+                                    </HStack>
+                                </HStack>
+                            </Item>
+                        </ItemGroup>
                         <SyntaxHighlighter language="json" wrapLongLines={true} customStyle={{ margin: 0 }}>
                             {JSON.stringify(sampleBluePrint, null, "\t")}
                         </SyntaxHighlighter>
